724bda7b2a2c817b3fb7b9c468e2a2b607466ea2
Marcin <marcin.krasowski@gmail.com>
2018-10-29 01:20:06 +0100

    getToken done

--- src/main/groovy/Parser.groovy
+++ src/main/groovy/Parser.groovy
@@ -1,23 +1,35 @@
+import groovy.transform.EqualsAndHashCode
+
+import java.util.regex.Matcher
+
 class Parser {
 
     def static parse(String formula){
-        def res = [:]
+        def tokens = []
+        Token t
+        String res
+        while (((t, res) = getToken(formula)))
 
         return res
     }
 
-    class Element implements Token {
-        String name
-        Integer number = 1
-    }
 
     interface Token {}
 
-    Token getToken(String formula) {
-        return null
+    static Tuple2<Token,String> getToken(String formula) {
+        Token token
+        String res = formula
+        def elementMatcher = ~$/^(\p{javaUpperCase}\p{javaLowerCase}?)(\p{Digit}?)/$
+        Matcher match
+        if((match = elementMatcher.matcher(formula)) || match.matches()){
+            def multiplyer = match[0][2]
+            token = new Element(match[0][1],multiplyer?Integer.parseInt(multiplyer):1)
+            res = formula - match[0][0]
+        }
+        return new Tuple2<Token, String>(token, res)
     }
 
-    def sumUpTokens(Element[] elements) {
+    def static sumUpElements(Element[] elements) {
         def res = [:]
         for (Element element : elements) {
             if(null == res[element.name]) {
@@ -28,4 +40,20 @@ class Parser {
         }
         return res
     }
+
+
+    @EqualsAndHashCode
+    static class Element implements Token {
+        String name
+        Integer number = 1
+
+        Element(String name, Integer number) {
+            this.name = name
+            this.number = number==0?1:number
+        }
+
+        Element() {
+        }
+    }
+
 }
--- src/test/groovy/ParserTest.groovy
+++ src/test/groovy/ParserTest.groovy
@@ -1,6 +1,10 @@
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import javax.xml.bind.Element
+
+import static Parser.sumUpElements
+
 
 class ParserTest extends Specification {
 
@@ -12,7 +16,35 @@ class ParserTest extends Specification {
         formula || res
         ""      || [:]
         "O"     || ["O": 1]
-        "O2"     || ["O": 2]
-        "H2O"     || ["O": 2, "H":1]
+        "O2"    || ["O": 2]
+        "H2O"   || ["O": 2, "H": 1]
     }
+
+    def "test summing up elements"() {
+        given:
+        Parser.Element[] elements = [["O", 1], ["O", 1], ["Mg", 1]]
+        when:
+        def sum = sumUpElements(elements)
+        then:
+        sum == ["O": 2, "Mg": 1]
+    }
+
+    @Unroll
+    def "test simply cases of getToken"() {
+        expect:
+        new Tuple2<Parser.Element, String>(element, residue) == Parser.getToken(formula)
+        where:
+        formula      || element                                   | residue
+        ""           || null                                      | ""
+        "Mg2O3"      || new Parser.Element("Mg", 2) | "O3"
+        "Mg"         || new Parser.Element("Mg", 1) | ""
+        "Mg(OH)2"    || new Parser.Element("Mg", 1) | "(OH)2"
+        "(Mg(OH)2)2" || null                                      | "(Mg(OH)2)2"
+        "o3"         || null                                      | "o3"
+        "MgO"        || new Parser.Element("Mg", 1) | "O"
+        "O"          || new Parser.Element("O", 1)  | ""
+        "O2"         || new Parser.Element("O", 2)  | ""
+        "OMg"        || new Parser.Element("O", 1)  | "Mg"
+    }
+
 }
